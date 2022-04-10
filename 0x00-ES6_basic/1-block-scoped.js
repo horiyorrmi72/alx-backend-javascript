735b967c0faa2138d999d03aafaadf82f8771e96
yusuf ridwan <horiyorrmi72@gmail.com>
2022-04-10 11:47:45 +0000

    mend

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskblock(trueOrFalse) {
   let task, task2;
 
     if (trueOrFalse) {
-	 task = true;
-	 task2 = false;
+	let task = true;
+	let  task2 = false;
     }
 
     return [task, task2];
