02657dd022a6a47e99947425fd79cd07b8e9d021
yusuf ridwan <horiyorrmi72@gmail.com>
2022-04-15 08:03:33 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+function getListStudents() {
+  myDataSet = [{
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  },
+    {
+      id: 2,
+      firstName: "James",
+      location: "Columbia",
+    },
+    {
+      id: 5,
+      firstName: "Serena",
+      location: "San Francisco",
+    }];
+    return myDataSet;
+}
