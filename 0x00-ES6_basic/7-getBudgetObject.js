6abc0c2bc56a91883c7eae51194596317de002b1
Yusuf Ridwan <horiyorrmi72@gmail.com>
2022-05-11 07:08:53 +0100

    eslint

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,18 +1,18 @@
 // export default function getBudgetObject(income, gdp, capita) {
-    //     const budget = {
-    //       income: income,
-    //       gdp: gdp,
-    //       capita: capita,
-    //     };
-      
-    //     return budget;
-    //   }
-    //Modify the above function’s budget object to simply use the keyname instead.
-    export default function getBudgetObject(income, gdp, capita) {
-        const budget = {
-          income,
-          gdp,
-          capita,
-        };
-        return budget;
-      }
+//     const budget = {
+//       income: income,
+//       gdp: gdp,
+//       capita: capita,
+//     };
+
+//     return budget;
+//   }
+//Modify the above function’s budget object to simply use the keyname instead.
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+  return budget;
+}
