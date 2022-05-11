85a85c55c39121eeebceed184bb45179eb694979
Yusuf Ridwan <horiyorrmi72@gmail.com>
2022-05-11 06:45:52 +0100

    need xhmod

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,18 @@
+// export default function getBudgetObject(income, gdp, capita) {
+    //     const budget = {
+    //       income: income,
+    //       gdp: gdp,
+    //       capita: capita,
+    //     };
+      
+    //     return budget;
+    //   }
+    //Modify the above function’s budget object to simply use the keyname instead.
+    export default function getBudgetObject(income, gdp, capita) {
+        const budget = {
+          income,
+          gdp,
+          capita,
+        };
+        return budget;
+      }
