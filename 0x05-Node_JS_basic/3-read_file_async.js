012407ebc4255fd8ab7301d9d98d52da1d54e51f
Yusuf Ridwan <horiyorrmi72@gmail.com>
2022-06-04 13:14:46 +0100

    readfile asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,24 +1,26 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(Error("Cannot load database"));
+        reject(Error('Cannot load the database'));
         return;
       }
       const response = [];
       let msg;
 
-      const content = data.toString().split("\n");
+      const content = data.toString().split('\n');
 
       let students = content.filter((item) => item);
 
-      students = students.map((item) => item.split(","));
+      students = students.map((item) => item.split(','));
 
       const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
       msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
       console.log(msg);
+
+      response.push(msg);
 
       const fields = {};
       for (const i in students) {
@@ -31,10 +33,10 @@ function countStudents(path) {
 
       delete fields.field;
 
-      for (const key in Object.keys(fields)) {
+      for (const key of Object.keys(fields)) {
         msg = `Number of students in ${key}: ${
           fields[key].length
-        }, List: ${fields[key].join(", ")}`;
+        }. List: ${fields[key].join(', ')}`;
 
         console.log(msg);
 
