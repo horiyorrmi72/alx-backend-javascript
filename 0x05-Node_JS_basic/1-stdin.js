ca800c4c51ce8149e79c2b5773091d8698ef4a5b
Yusuf Ridwan <horiyorrmi72@gmail.com>
2023-08-03 07:29:18 +0000

    check end

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is ${chunk}`);
-  }
+  if (chunk) process.stdout.write(`Your name is: ${chunk}`);
 });
-
 process.stdin.on('end', () => {
-  process.stdoout.write('This important software is now closing \n');
+  process.stdout.write('This important software is now closing\n');
 });
