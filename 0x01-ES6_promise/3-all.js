806a16e74106f10ad2e217e2cf70dc76293ba464
yusuf <horiyorrmi72@gmail.com>
2024-08-18 10:07:11 +0100

    fixed linting ish

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,20 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup(){
-    let body;
-    let firstName;
-    let lastName;
+export default function handleProfileSignup() {
+  let body;
+  let firstName;
+  let lastName;
 
-    return uploadPhoto()
+  return uploadPhoto()
     .then((data) => {
-        body = data.body;
-        createUser()
-        .then((data) =>{
-            firstName = data.firstName;
-            lastName = data.lastName;
-            console.log(`${body} ${firstName} ${lastName}`);
-         })
-         .catch(() => console.log('Singup system offline'));
+      body = data.body;
+      createUser()
+        .then((data) => {
+          firstName = data.firstName;
+          lastName = data.lastName;
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Singup system offline'));
     })
-    .catch(()=> console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
