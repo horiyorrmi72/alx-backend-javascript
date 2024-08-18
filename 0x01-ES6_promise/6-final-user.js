e3db964ef5ebc382084a77d1e706a9e7c0800c57
yusuf <horiyorrmi72@gmail.com>
2024-08-18 10:22:44 +0100

    fixed linting ish

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,20 @@
-import signUpUser from '/4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName,lastName,filename){
-    const userData = await signUpUser(firstName,lastName).then((data) => ({
-        status: 'fulfilled',
-        value: data,
-    }));
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  filename,
+) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
 
-    const fileData = await uploadPhoto(filename).catch((err) => ({
-        status:'rejected',
-        value: err.toString(),
-    }));
-    
-    return Promise.resolve([userData,fileData])
+  const fileData = await uploadPhoto(filename).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
 }
