db5c9ffd02546fd7a91d0f46743d80458816146a
yusuf <horiyorrmi72@gmail.com>
2024-08-19 10:36:46 +0100

    fixed linting ish

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
-    const sizes = [19,20,34];
-    const classArray = [];
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const classArray = [];
 
-    for(const size of sizes){
-        classArray.push(new ClassRoom(size));
-    }
-    return classArray
+  for (const size of sizes) {
+    classArray.push(new ClassRoom(size));
+  }
+  return classArray;
 }
