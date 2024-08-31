59907ab5ce2613ef40080513ba2b38718722e890
yusuf <horiyorrmi72@gmail.com>
2024-08-31 21:31:44 +0100

    money[currency]

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,25 +1,26 @@
-export default class Currency{
-    constructor(code,name){
-        this._code = code;
-        this._name = name;
-    }
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
+  set name(newName) {
+    this._name = newName;
+  }
 
-    displayFullCurrency(){
-        return (`${this.name} (${this.code})`);
-    }
+  set code(newCode) {
+    this._code = newCode;
+  }
 
-    set name(newName){
-        this._name = newName
-        }
-    set code(newCode){
-        this._code = newCode;
-    }
-    get name(){
-        return this._name;
-    }
-    get code(){
-        return this._code;
-    }
+  get name() {
+    return this._name;
+  }
 
+  get code() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
