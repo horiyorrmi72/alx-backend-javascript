59174f77844288ea6048238497473651763a73cb
yusuf <horiyorrmi72@gmail.com>
2024-09-06 12:12:14 +0100

    double checking type of data

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0 || typeof startString !== 'string') return '';
+  if (typeof startString !== 'string' || startString.length === 0 ) return '';
 
   let result = '';
   set.forEach((value) => {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result += `${value.slice(startString.length)}-`;
     }
   });
 
-  return result.slice(0, -1);
+  return result ? result.slice(0, -1) : '';
 }
