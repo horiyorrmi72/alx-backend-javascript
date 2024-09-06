8132243e523c1a13eee0056146a3d1836b51eb84
yusuf <horiyorrmi72@gmail.com>
2024-09-06 11:59:04 +0100

    using forEach and slice method

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) return '';
+
+  let result = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  });
+  return result.slice(0, -1);
+}
