017d680f453563cb2603e2c93a38621a214f72df
yusuf <horiyorrmi72@gmail.com>
2024-09-20 07:06:50 +0100

    fix(server.js): removed file ext

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,7 +1,6 @@
-import app from './routes/index.js';
+import app from './routes/index';
 
 const host = '127.0.0.1';
 const port = 1245;
-
 app.listen(port, () => console.log(`app running at http://${host}:${port}/`));
 export default app;
