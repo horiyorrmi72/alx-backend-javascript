45041ecf91459d944c9b79ac155108d81b1cb11e
yusuf <horiyorrmi72@gmail.com>
2024-09-28 12:53:00 +0100

    [feat]: added more test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,10 @@ const calculateNumber = require('./0-calcul');
 
 describe('rounded numbers', function () {
 	it('checks if it rounds the number', function () {
+		assert.equal(calculateNumber(1.2, 3.7), 5);
+		assert.equal(calculateNumber(2.7, 4.2), 7);
+		assert.equal(calculateNumber(1.5, 3.7), 6);
+		assert.equal(calculateNumber(2.4, 4.8), 7);
 		assert.strictEqual(calculateNumber(2.5, 5), 8);
 		assert.strictEqual(calculateNumber(0.5, 1), 2);
 		assert.strictEqual(calculateNumber(2.5, 6), 9);
