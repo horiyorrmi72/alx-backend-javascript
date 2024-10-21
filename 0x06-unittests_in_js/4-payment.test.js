fd692e18d71c1bcb3a4d1205c080239663c5f2cd
Yusuf Ridwan A. <horiyorrmi72@gmail.com>
2024-10-21 17:42:07 +0100

    Update 4-payment.test.js(formatting)

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,19 +4,25 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-	it('stubs Utils.calculateNumber to always return 10 and verifies the arguments', () => {
-		const calculateNumberStub = sinon
-			.stub(Utils, 'calculateNumber')
-			.returns(10);
+    it('stubs Utils.calculateNumber to always return 10 and verifies the arguments', () => {
+        
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
+        
         const consoleSpy = sinon.spy(console, 'log');
+
+        
         sendPaymentRequestToApi(100, 20);
 
-		expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-		expect(calculateNumberStub.calledOnce).to.be.true;
-		expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
+        
+        expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+        expect(calculateNumberStub.calledOnce).to.be.true;
 
-		calculateNumberStub.restore();
-		consoleSpy.restore();
-	});
+        
+        expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
+
+      
+        calculateNumberStub.restore();
+        consoleSpy.restore();
+    });
 });
