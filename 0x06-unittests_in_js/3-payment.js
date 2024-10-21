6a0eecec7ecc0fea70aae4a79e5b553c4f0925b5
yusuf <horiyorrmi72@gmail.com>
2024-10-21 10:03:48 +0100

    return value

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,8 +2,11 @@ const Utils = require('./utils');
 
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const sumTotal = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	const sumTotal = Utils.calculateNumber('SUM', totalAmount, totalShipping);
 
     console.log(`The total is :${sumTotal}`);
+    return sumTotal;
 }
+
+// console.log('from payment-gateway:', sendPaymentRequestToApi(100, 10));
 module.exports = sendPaymentRequestToApi;
